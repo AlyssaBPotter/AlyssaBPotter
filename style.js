3387e355d85e8e88eb7910dde6c4ed16be6b3026
Alyssa B Potter Website <114842431+AlyssaBPotter@users.noreply.github.com>
2022-10-12 09:02:45 -0700

    Add files via upload

--- style.js
+++ style.js
@@ -1,2 +1,14 @@
 // external js: flickity.pkgd.js, fullscreen.js
 
+function noSpam() {
+    // Get the text field
+    var emailOne = "alys";
+    var emailTwo = "otterwr";
+    var emailThree = "gmail.com";
+
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(emailOne+"sap"+emailTwo+"ites@"+emailThree);
+  
+    // Alert the copied text
+    alert("Email Copied" + copyText.value);
+  } 
